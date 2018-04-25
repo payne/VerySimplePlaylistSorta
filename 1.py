e4c697aa0053fa13aa4983eb31a3d121f0332eaa
Matt Payne <Payne@MattPayne.org>
2018-04-24 21:13:09 -0500

    1.py outputs a shell script of wgets for current NPR ATC stories.

--- 1.py
+++ 1.py
@@ -7,11 +7,12 @@ url = "https://www.npr.org/programs/all-things-considered/"
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
 audioLabels = soup.find_all(class_='audio-tool-label')
+n=1
 for a in audioLabels: 
   if (a.text == 'Download'):
     p=a.parent
     href=p.get('href')
-    print(href)
-
-
+    href=href.split('?')[0]
+    print("wget -O %d.mp3 %s" % (n, href))
+    n += 1
 
